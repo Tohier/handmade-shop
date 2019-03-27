e9bac6625c642abbeaa155ae9de3f5302c370bf8
Tohier <tohierdreyer17@gmail,com>
2019-03-27 12:33:27 +0200

    started on the shop page;

--- js/main.js
+++ js/main.js
@@ -7,3 +7,24 @@ $(function () {
     })
 });
 
+var back_to_top_button = jQuery(".btn-back-to-top");
+
+jQuery(window).scroll(function() {
+  if (
+    jQuery(this).scrollTop() > 100 &&
+    !back_to_top_button.hasClass("scrolled")
+  ) {
+    back_to_top_button.addClass("scrolled");
+  } else if (
+    jQuery(this).scrollTop() < 100 &&
+    back_to_top_button.hasClass("scrolled")
+  ) {
+    back_to_top_button.removeClass("scrolled");
+  }
+});
+
+//click to scroll to top
+back_to_top_button.click(function() {
+  jQuery("html, body").animate({ scrollTop: 0 }, 800);
+  return false;
+});
