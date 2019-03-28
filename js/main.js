946d8548babccc42a7cf851257edb1d934c7cabb
Tohier <tohierdreyer17@gmail,com>
2019-03-28 09:37:16 +0200

    added a back to top

--- js/main.js
+++ js/main.js
@@ -6,3 +6,21 @@ $(function() {
     console.log("Requested for showing");
   });
 });
+
+// the code for the back to top
+$(window).scroll(function () {
+  var height = $(window).scrollTop();
+  if (height > 100) {
+    $('#back2Top').fadeIn();
+  } else {
+    $('#back2Top').fadeOut();
+  }
+});
+$(document).ready(function () {
+  $("#back2Top").click(function (event) {
+    event.preventDefault();
+    $("html, body").animate({ scrollTop: 0 }, "slow");
+    return false;
+  });
+
+});
